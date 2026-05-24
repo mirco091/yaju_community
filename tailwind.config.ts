21026e5b7131df7dd28806aa20cbddc54c9dbd11
miro <fumiya116059@gmail.com>
2026-05-24 17:45:58 +0900

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-import type { Config } from 'tailwindcss'
-const config: Config = { content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'], theme: { extend: { boxShadow: { glow: '0 0 35px rgba(168,85,247,.45)' } } }, plugins: [] }
-export default config
