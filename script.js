d55816fde2add8c7234530443e8fe08dd5681030
miro <fumiya116059@gmail.com>
2026-05-24 18:13:43 +0900

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll('button').forEach(btn=>btn.addEventListener('click',()=>{btn.animate([{transform:'scale(1)'},{transform:'scale(.94)'},{transform:'scale(1)'}],{duration:220})}))
