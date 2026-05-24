3a6ab276daf42be6982d4fb569199ace733a09bb
miro <fumiya116059@gmail.com>
2026-05-24 17:46:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('button').forEach(btn=>btn.addEventListener('click',()=>{btn.animate([{transform:'scale(1)'},{transform:'scale(.94)'},{transform:'scale(1)'}],{duration:220})}))
